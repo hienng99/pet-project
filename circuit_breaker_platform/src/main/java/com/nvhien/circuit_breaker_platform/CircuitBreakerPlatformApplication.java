2dba1d7ffa176e814e3128db4d12e3d7717c825d
Nguyễn Vinh Hiển <nguyenvinhhien19061999@gmail.com>
2024-01-10 20:51:01 +0700

    update circuit breaker

---
+++ circuit_breaker_platform/src/main/java/com/nvhien/circuit_breaker_platform/CircuitBreakerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.nvhien.circuit_breaker_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitBreakerPlatformApplication.class, args);
+    }
+
+}
---
+++ circuit_breaker_platform/src/test/java/com/nvhien/circuit_breaker_platform/CircuitBreakerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvhien.circuit_breaker_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
