7c7a978f02f0a6c2037f872fe50d88152b1f8521
Nguyễn Vinh Hiển <nguyenvinhhien19061999@gmail.com>
2024-01-14 22:00:27 +0700

    cb

--- circuit_breaker_platform/src/main/java/com/nvhien/circuit_breaker_platform/service/CircuitBreakerService.java
+++ circuit_breaker_platform/src/main/java/com/nvhien/circuit_breaker_platform/service/CircuitBreakerService.java
@@ -2,55 +2,50 @@ package com.nvhien.circuit_breaker_platform.service;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @Service
 @Slf4j
 public class CircuitBreakerService {
-    CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
-            .failureRateThreshold(50)
-            .slowCallRateThreshold(50)
-            .waitDurationInOpenState(Duration.ofMinutes(5))
-            .slowCallDurationThreshold(Duration.ofSeconds(30))
-            .permittedNumberOfCallsInHalfOpenState(3)
-            .minimumNumberOfCalls(3)
-            .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
-            .slidingWindowSize(3)
-            .recordExceptions(IOException.class, TimeoutException.class)
-            .build();
-
-    CircuitBreakerRegistry circuitBreakerRegistry = CircuitBreakerRegistry.of(circuitBreakerConfig);
-
-    CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker("my_cb", circuitBreakerConfig);
+    private static CircuitBreaker circuitBreakerInstant;
 
     public void handleResultCode(int resultCode) {
+        CircuitBreaker circuitBreaker = getCB();
         if (resultCode == 0) {
             log.info("CB on success.");
             circuitBreaker.onSuccess(1, TimeUnit.SECONDS);
         } else {
             log.info("CB on error.");
-            circuitBreaker.onError(1, TimeUnit.SECONDS, new Throwable());
+            circuitBreaker.onError(100, TimeUnit.SECONDS, new Throwable());
         }
-        circuitBreaker.getEventPublisher().onStateTransition(event -> {
+        CircuitBreaker.EventPublisher eventPublisher = circuitBreaker.getEventPublisher();
+        eventPublisher.onStateTransition(event -> {
             CircuitBreaker.StateTransition stateTransition = event.getStateTransition();
-            switch (stateTransition.getToState()) {
-                case OPEN -> {
-                    // call maintenance
-                    log.error("OPEN CB");
-                }
-                case HALF_OPEN -> {
-                    // call unblock
-                    log.error("HALF OPEN CB");
-                }
+            if (stateTransition.getToState() == CircuitBreaker.State.OPEN) {
+                log.error("OPEN CB");
+            } else if (stateTransition.getToState() == CircuitBreaker.State.HALF_OPEN) {
+                log.error("HALF OPEN CB");
             }
         });
+    }
+
+    private synchronized CircuitBreaker getCB() {
+        if (circuitBreakerInstant == null) {
+            CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                    .failureRateThreshold(50)
+                    .waitDurationInOpenState(Duration.ofSeconds(1))
+                    .permittedNumberOfCallsInHalfOpenState(3)
+                    .minimumNumberOfCalls(3)
+                    .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                    .slidingWindowSize(3)
+                    .build();
+
+            circuitBreakerInstant = CircuitBreaker.of("my_circuit_breaker", circuitBreakerConfig);
+        }
+        return circuitBreakerInstant;
     }
 }
